7ff15d360e0758362060f44c4485a49df53d3ba1
glynnforrest <me@glynnforrest.com>
2013-06-26 18:03:36 +0100

    Adding png support to GDAdapter.

--- src/Pikasso/Adapters/GDAdapter.php
+++ src/Pikasso/Adapters/GDAdapter.php
@@ -2,6 +2,8 @@
 
 namespace Pikasso\Adapters;
 
+use Pikasso\Pikasso;
+
 /**
  * GDAdapter
  * @author Glynn Forrest <me@glynnforrest.com>
@@ -15,14 +17,59 @@ class GDAdapter extends Adapter {
 		//create a blank image with the given dimensions
 		$new_image = imagecreatetruecolor($this->width, $this->height);
 		//open the original image to resize from
-		$image = imagecreatefromjpeg($this->filename);
+		$image = $this->imageCreateFromFormat($this->filename);
+
+		//if the image is a gif or png, allow for transparency
+		if (($this->format == Pikasso::FORMAT_GIF) ||
+			($this->format == Pikasso::FORMAT_PNG)) {
+			imagealphablending($new_image, false);
+			imagesavealpha($new_image, true);
+			$transparent = imagecolorallocatealpha($new_image,
+												   255, 255, 255, 127);
+			imagefilledrectangle($new_image, 0, 0,
+								 $this->width, $this->height,
+								 $transparent);
+		}
+
 		//resample the original into the new image
 		imagecopyresampled($new_image, $image, 0, 0, 0, 0,
 						   //target width and height
 						   $this->width, $this->height,
 						   //original width and height
 						   $this->info[0], $this->info[1]);
-		//save new image to $filename
-		return imagejpeg($new_image, $filename);
+
+		//save new image to $filename, accounting for format
+		return $this->imageSaveFormat($new_image, $filename);
 	}
+
+	protected function imageCreateFromFormat($filename) {
+		switch ($this->format) {
+		case Pikasso::FORMAT_JPEG:
+			return imagecreatefromjpeg($filename);
+		case Pikasso::FORMAT_PNG:
+			return imagecreatefrompng($filename);
+		default:
+			throw new PikassoException(
+				'Unsupported format given to GDAdapter::imageCreateFromFormat : '
+				. $this->format);
+		}
+	}
+
+	/**
+	 * Save an image resource to $filename, using the appropriate
+	 * function for the given format.
+	 */
+	protected function imageSaveFormat($resource, $filename) {
+		switch ($this->format) {
+		case Pikasso::FORMAT_JPEG:
+			return imagejpeg($resource, $filename);
+		case Pikasso::FORMAT_PNG:
+			return imagepng($resource, $filename);
+		default:
+			throw new PikassoException(
+				'Unsupported format given to GDAdapter::imageSaveFormat : '
+				. $this->format);
+		}
+	}
+
 }
--- tests/Pikasso/Adapters/GDAdapterTest.php
+++ tests/Pikasso/Adapters/GDAdapterTest.php
@@ -16,23 +16,34 @@ class GDAdapterTest extends \PHPUnit_Framework_TestCase {
 	public function testGetWidth() {
 		$adapter = new GDAdapter(TEST_JPEG_FILE);
 		$this->assertEquals(600, $adapter->getWidth());
+		$adapter = new GDAdapter(TEST_PNG_FILE);
+		$this->assertEquals(297, $adapter->getWidth());
 	}
 
 	public function testGetHeight() {
 		$adapter = new GDAdapter(TEST_JPEG_FILE);
 		$this->assertEquals(450, $adapter->getHeight());
+		$adapter = new GDAdapter(TEST_PNG_FILE);
+		$this->assertEquals(400, $adapter->getHeight());
 	}
 
 	public function testSetWidth() {
 		$adapter = new GDAdapter(TEST_JPEG_FILE);
+		//assert that method chaining is possible
 		$this->assertTrue($adapter->setWidth(300) instanceof GDAdapter);
 		$this->assertEquals(300, $adapter->getWidth());
+		$adapter = new GDAdapter(TEST_PNG_FILE);
+		$this->assertTrue($adapter->setWidth(100) instanceof GDAdapter);
+		$this->assertEquals(100, $adapter->getWidth());
 	}
 
 	public function testSetHeight() {
 		$adapter = new GDAdapter(TEST_JPEG_FILE);
 		$this->assertTrue($adapter->setHeight(225) instanceof GDAdapter);
 		$this->assertEquals(225, $adapter->getHeight());
+		$adapter = new GDAdapter(TEST_PNG_FILE);
+		$this->assertTrue($adapter->setHeight(200) instanceof GDAdapter);
+		$this->assertEquals(200, $adapter->getHeight());
 	}
 
 	public function testSaveJpeg() {
@@ -47,15 +58,33 @@ class GDAdapterTest extends \PHPUnit_Framework_TestCase {
 		@unlink($small_file);
 	}
 
+	public function testSavePng() {
+		$adapter = new GDAdapter(TEST_PNG_FILE);
+		$adapter->setHeight(200)->setWidth(100);
+		$small_file = TEST_PNG_FILE . '.small';
+		$adapter->save($small_file);
+		$this->assertTrue(file_exists($small_file));
+		$small_adapter = new GDAdapter($small_file);
+		$this->assertEquals(100, $small_adapter->getWidth());
+		$this->assertEquals(200, $small_adapter->getHeight());
+		@unlink($small_file);
+	}
+
+
 	public function testGetFormat() {
 		$adapter = new GDAdapter(TEST_JPEG_FILE);
 		$this->assertEquals(Pikasso::FORMAT_JPEG, $adapter->getFormat());
+		$adapter = new GDAdapter(TEST_PNG_FILE);
+		$this->assertEquals(Pikasso::FORMAT_PNG, $adapter->getFormat());
 	}
 
 	public function testSetFormat() {
 		$adapter = new GDAdapter(TEST_JPEG_FILE);
 		$this->assertTrue($adapter->setFormat(Pikasso::FORMAT_PNG) instanceof GDAdapter);
 		$this->assertEquals(Pikasso::FORMAT_PNG, $adapter->getFormat());
+		$adapter = new GDAdapter(TEST_PNG_FILE);
+		$this->assertTrue($adapter->setFormat(Pikasso::FORMAT_JPEG) instanceof GDAdapter);
+		$this->assertEquals(Pikasso::FORMAT_JPEG, $adapter->getFormat());
 	}
 
 }
